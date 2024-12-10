895752ca5d139a627fcb789fe386f1da464813f8
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-10 12:39:37 -0300

    se trabaja en ejemplos kivy

--- ejemplos/kivy_app.py
+++ ejemplos/kivy_app.py
@@ -3,3 +3,9 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.label import Label
 from kivy.uix.textinput import TextInput
 from kivy.uix.button import Button
+
+class MyApp(App):
+    def build(self):
+        layout = BoxLayout(orientation='vertical')
+        layout.add_widget(Label(text="Hola Mundo"))
+        self.text_input = TextInput()
