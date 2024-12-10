2b3235724d3628f8606f33dba5af2994a1d1bce7
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-10 12:34:41 -0300

    se trabaja en ejemplos pyqt

--- ejemplos/pyqt_app.py
+++ ejemplos/pyqt_app.py
@@ -1,1 +1,5 @@
 from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QLineEdit, QPushButton
+
+def enviar():
+    mensaje = entrada.text()
+    print(f"Mensaje ingresado: {mensaje}")
