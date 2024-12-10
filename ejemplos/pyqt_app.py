d177de8719985169603b7e569b53de295d55b3e2
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-10 12:34:58 -0300

    se trabaja en ejemplos pyqt

--- ejemplos/pyqt_app.py
+++ ejemplos/pyqt_app.py
@@ -3,3 +3,7 @@ from PyQt5.QtWidgets import QApplication, QVBoxLayout, QWidget, QLabel, QLineEdi
 def enviar():
     mensaje = entrada.text()
     print(f"Mensaje ingresado: {mensaje}")
+
+app = QApplication([])
+ventana = QWidget()
+ventana.setWindowTitle("Ejemplo PyQt")
