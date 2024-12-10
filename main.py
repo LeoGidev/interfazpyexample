0aca99e8416248d850e5b227c383cd33226fb1ce
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-10 12:23:13 -0300

    se trabaja en main

--- main.py
+++ main.py
@@ -1,2 +1,9 @@
 import subprocess
 import sys
+
+def ejecutar_ejemplo(nombre):
+    """Ejecuta el archivo de ejemplo correspondiente"""
+    try:
+        subprocess.Popen([sys.executable, f"ejemplos/{nombre}_app.py"])
+    except Exception as e:
+        print(f"Error al abrir {nombre}: {e}")
