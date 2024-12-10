b76fb060441ff0ac457b3c12d52dd8ee12cd1209
Leo Gimenez <leogimenez.dev@gmail.com>
2024-12-10 12:29:43 -0300

    se trabaja en main

--- main.py
+++ main.py
@@ -7,9 +7,14 @@ def ejecutar_ejemplo(nombre):
         subprocess.Popen([sys.executable, f"ejemplos/{nombre}_app.py"])
     except Exception as e:
         print(f"Error al abrir {nombre}: {e}")
+
 if __name__ == "__main__":
     print("Selecciona el ejemplo:")
     print("1. Tkinter")
     print("2. PyQt")
     print("3. Kivy")
     print("4. wxPython")
+
+    opcion = input("Ingresa el número del ejemplo: ")
+    opciones = {"1": "tkinter", "2": "pyqt", "3": "kivy", "4": "wxpython"}
+    ejemplo = opciones.get(opcion)
